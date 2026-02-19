a8f89f591097bf40b27a2614a3f6ae3d1ebab77e
Kouki-kasagawa <benkyopui5420@outlook.jp>
2026-02-19 23:39:27 +0900

    ローカル上でhtmlファイルを表示できるようにした

--- server.js
+++ server.js
@@ -1,1 +1,22 @@
-console.log("nodejs");
+const http = require("http");
+const PORT = 8080;
+const html = require("fs").readFileSync("./index.html");
+
+//webサーバを作ろう
+const server = http.createServer((req, res) => {
+    //ブラウザからアクセスが来たときの処理
+    res.writeHead(200, {"Content-Type" : "text/html"});
+    res.write(html);
+    res.end();
+
+    if (req.method == "GET"){
+    }
+
+    if (req.method =="POST"){
+        
+    }
+});
+
+server.listen(PORT, () => {
+    console.log("server running")
+});

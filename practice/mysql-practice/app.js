6d1d7b50e89b15cbce2feb3794209ef663aa749a
Kouki-kasagawa <benkyopui5420@outlook.jp>
2026-02-26 00:32:01 +0900

    Node.jsを使って、SQLをかけるようにしました

--- practice/mysql-practice/app.js
+++ practice/mysql-practice/app.js
@@ -1,21 +1,28 @@
+require('dotenv').config();
 const mysql = require("mysql2");
 
 const connection = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "password",
-    database: "practice_db"
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
 });
 
+connection.query("SELECT DATABASE()", (err, results) => {
+    console.log(results);
+});
 
 console.log("クエリ実行前");
 
 connection.query(
-    "INSERT INTO user (name) VALUES (?)",
-    ["Hanachan"],
+    "INSERT INTO users (name) VALUES (?)",
+    ["Taro"],
     (err, results) => {
-    console.log("クエリ実行後");
-    console.log(results);
+    if (err) {
+        console.error("エラー:", err);
+        return;
+    }
+    console.log("成功:", results);
     }
 );
 
